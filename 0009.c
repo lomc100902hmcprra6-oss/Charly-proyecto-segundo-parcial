e3838e09b827536acf8f36d6dc3d14ab3ee2cfd1
lomc100902hmcprra6-oss <lomc100902hmcprra6@lake.edu.mx>
2025-11-11 12:48:41 -0600

    Update 0009.c

--- 0009.c
+++ 0009.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-main(){
+main()
+//te da el calculo de una hipotenusa//
+{
 float Cateto1;
 float Cateto2;
 float Hipotenusa;
@@ -14,3 +16,4 @@ Hipotenusa =  sqrt((Cateto1*Cateto1)+(Cateto2*Cateto2));
 
 printf("La hipotenusa es: %.2f \n", Hipotenusa);
 }
+
