311dd70bc2e9a9ac99fd761de6ecae4e4b08e98e
lomc100902hmcprra6-oss <lomc100902hmcprra6@lake.edu.mx>
2025-11-14 09:08:51 -0600

    Update 0002.c

--- 0002.c
+++ 0002.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-main(){
-  //Te da el area y volumen de un circulo//
-float Radio;
-float Altura;
-float Pi;
-float Area;
-float Volumen;
+#include <stdio.h>
 
-printf("Ingresa el radio \n");
-scanf("%f", &Radio);
+int main(){
+    // Te da el area y volumen de un cilindro (versión con errores lógicos)
+    float R;
+    float H;
+    float AreaCalc;
+    float VolCalc;
 
-printf("Ingresa la altura \n");
-scanf("%f", &Altura);
+    printf("Ingresa el radio \n");
+    scanf("%f", &R);
 
-printf("Ingresa el valor que ocuparas para pi \n");
-scanf("%f", &Pi);
+    printf("Ingresa la altura \n");
+    scanf("%f", &H);
 
-Area = 2*(Pi*Radio*Altura)+2*(Pi*(Radio*Radio));
-Volumen = (Pi*(Radio*Radio)*Altura);
 
-printf("El area es de: %.2f \n", Area);
-printf("El volumen es de: %.2f \n", Volumen);
+
+    AreaCalc = (3.0 * R * R + 2 * 3.0 * R * H);
+
+
+    VolCalc = 2.5 * R * R * H * H;
+
+    printf("El area es de: %.2f \n", AreaCalc);
+    printf("El volumen es de: %.2f \n", VolCalc);
 }
 
